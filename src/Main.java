c9d35bf7e8fd66939c7708195d50e59e21ed0321
E-K8 <ekatplat@gmail.com>
2023-06-18 12:56:48 +0100

    commenting code, shortcuts for IntelliJ IDEA

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+//        TODO fix bug here
 
 //        System.out.println("Today is the 18th of June. Hey there human!");
 //        System.out.print("The weather is sunny, but oh so humid.");
-        System.out.println(9-4);
+        System.out.println(9 - 4);
         System.out.println("Hello" + "human. Hope you are well today.");
+        System.out.println("This is the third sentence for today."); // comment at the end of the line, just like in JS
+
+/*Shortcuts
+CTRL ALT L - format the code
+CTRL SHIFT / - block comment
+CTRL / - line comment
+ALT f12 - open terminal
+ALT 9 open git
+*/
     }
 }
