ae5bb416bd49bf3191bf8a0da4d8d8e4e2f3ab32
E-K8 <ekatplat@gmail.com>
2023-06-24 22:28:27 +0100

    assignment operators

---
+++ src/Assignment.java
@@ -0,0 +1,17 @@
+public class Assignment {
+    /*
+    =, +=, -=, /=, *=
+     */
+    public static void main(String[] args) {
+        int x = 8;
+        int y = x;
+        x++;
+//        x+=2;
+//        x-=3;
+//        x/=2;
+//        x*=3;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -62,6 +62,7 @@ ALT 4 - show/hide run tab
 ALT 9 - show/hide git tab
 CTRL SHIRT A - option to toggle soft wrap
 CTRL SHIRT F12 - show/hide all tool windows
+CTRL D - duplicate a line of code
 
 */
     }
