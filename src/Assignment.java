c5d566c2285d62611d47c18a1c60dce9ddaae250
E-K8 <ekatplat@gmail.com>
2023-06-25 16:13:12 +0100

    boolean values and relational operators

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,8 +1,16 @@
 public class Assignment {
     /*
-    =, +=, -=, /=, *=
+    Assignment operators: =, +=, -=, /=, *=
+    Boolean values: true/false;
+    Relational operators: ==, !=, >, >=, <, <=
      */
     public static void main(String[] args) {
+        boolean isActive = true;
+        System.out.println(isActive);
+        System.out.println(5 != 5);
+        System.out.println(5 >= 3);
+
+
         int x = 8;
         int y = x;
         x++;
