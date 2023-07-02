c01c5a6132a582a53a56f4269bf42053a77b07cd
E-K8 <ekatplat@gmail.com>
2023-07-02 20:40:00 +0100

    check number for being negative of positive

---
+++ src/NumberChecker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumberChecker {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        if ( in.hasNextInt() ){
+            int num = in.nextInt();
+
+            if (num < 0) {
+                System.out.println("You gave me a negative number");
+            } else {
+                System.out.println("You gave me a positive number");
+            }
+        } else {
+            System.out.println("Please enter an integer");
+        }
+    }
+}
