4a1e1871f49152b3b366339755c01b2c99f2410e
E-K8 <ekatplat@gmail.com>
2023-07-05 07:26:13 +0100

    logical operators and their combinations

---
+++ src/LogicalCombine.java
@@ -0,0 +1,29 @@
+public class LogicalCombine {
+    public static void main(String[] args) {
+     /*   if (true && true) {
+            System.out.println("block executed");
+        }
+        if (true && false) {
+            System.out.println("block unexecuted");
+        }
+        if (false && true) {
+            System.out.println("block unexecuted");
+        }
+        if (false && false) {
+            System.out.println("block unexecuted");
+        }
+        */
+        if (true || true) {
+            System.out.println("block executed");
+        }
+        if (true || false) {
+            System.out.println("block executed");
+        }
+        if (false || true) {
+            System.out.println("block executed");
+        }
+        if (false || false) {
+            System.out.println("block unexecuted");
+        }
+    }
+}
