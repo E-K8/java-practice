b3a539631a90db38ae3a2c9c94dd144b1de9e6b8
E-K8 <ekatplat@gmail.com>
2023-07-08 17:14:01 +0100

    intro to debugging

---
+++ src/DebugIntro.java
@@ -0,0 +1,17 @@
+public class DebugIntro {
+    public static void main(String[] args) {
+        int b = 35;
+        int a = 6;
+        int c = a/b;
+        int d = b/a;
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
+
+/*
+Alt 5       open debugger
+Shift F9    debug
+F8          step over
+*/
