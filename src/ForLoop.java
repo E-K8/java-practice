14081230ff9eb7b66b335251ed810064b77e8af9
E-K8 <ekatplat@gmail.com>
2023-07-10 17:03:28 +0100

    variations of for loop

---
+++ src/ForLoop.java
@@ -0,0 +1,35 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int incCounter = 0; incCounter <= 5; incCounter++) {
+            System.out.println(incCounter);
+        }
+
+        System.out.println("break line between counters");
+
+        for (int decCounter = 5; decCounter >= 1; decCounter--) {
+            System.out.println(decCounter);
+        }
+
+        System.out.println("break line between counters");
+
+        for (int plusTwo = 5; plusTwo <= 10; plusTwo += 2) {
+            System.out.println(plusTwo); // 5,7,9
+        }
+
+        System.out.println("break line between counters");
+
+        for (int counter = 1, x = 1; counter <= 10 && x < 3; counter += 2, x++) {
+            System.out.println("counter: " + counter + " , x: " + x);
+        }
+
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 5; j++) {
+//
+//            }
+//        }
+
+        for (char myChar='a'; myChar<='z'; myChar++) {
+            System.out.println(myChar);
+        }
+    }
+}
