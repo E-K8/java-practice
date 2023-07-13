e04ae482e70bb348467acef9187854bcdd5ea52c
E-K8 <ekatplat@gmail.com>
2023-07-13 11:56:43 +0100

    multiplication table of 6; print max number of three entered by user

--- src/IterationsPractice.java
+++ src/IterationsPractice.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class IterationsPractice {
 //    1. Find the sum of the first 5 natural numbers
 //    2. Read 5 user inputs and print their total and average (using accumulator pattern)
+//    3. Print the multiplication table of 6
+//    4. Read 3 numbers from the user and print the max number
 
     public static void main(String[] args) {
 //        1.
@@ -17,6 +19,7 @@ public class IterationsPractice {
         */
 
 //        2.
+/*
         Scanner in = new Scanner(System.in);
         int total = 0;
 
@@ -29,5 +32,29 @@ public class IterationsPractice {
 
         double avg = total / 5.0;
         System.out.printf("Average: %.2f", avg);
+        */
+
+//        3.
+/*
+
+        for (int i = 1; i <= 12; i++) {
+            int result = 6*i;
+            System.out.printf("6 X %d = %d\n", i, result);
+        }
+*/
+
+//        4.
+        Scanner in = new Scanner(System.in);
+        int maxNum = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int currNum = in.nextInt();
+
+            if (currNum > maxNum) {
+                maxNum = currNum;
+            }
+        }
+        System.out.println("Maximum number is " + maxNum);
+
     }
 }
