24294a5620b71f46bee1b889f98ea47680a9ff62
E-K8 <ekatplat@gmail.com>
2023-07-15 19:58:47 +0100

    print pattens with numbers and asterisks

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,6 +1,10 @@
 public class NestedLoops {
 //    1. Print nested loops separated by lines
 //    2. Print multiplication table for numbers 1,2,3,4,5
+//    3. Print pattern 1 22 333 444 55555, each number or number sequence to be stacked under the previous one
+//    4. Print pattern ***** **** *** ** *,each sequence of asterisks to be stacked under the previous one
+//    5. Print a fir tree pattern with asterisks (one, two, three, four, five asterisks stacked in the form or a tree)
+
 
 /*
     public static void main(String[] args) {
@@ -15,6 +19,7 @@ public class NestedLoops {
     }
     */
 
+    /*
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("Times table for number: " + i);
@@ -22,6 +27,56 @@ public class NestedLoops {
                 System.out.println(i + " X " + j + " = " + (i * j));
             }
             System.out.println("-------------------");
+        }
+    }
+    */
+
+//    3.
+
+    /*
+        public static void main(String[] args) {
+            for (int i = 1; i <=5; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(i);
+                }
+                System.out.println();
+            }
+        }
+        */
+
+//    4.
+  /*
+  public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    */
+
+//    5.
+
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+
+//            first inner loop: print spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+//            second inner loop (print stars)
+            for (int j = 0; j <=5-i; j++) {
+                System.out.print("* ");
+            }
+
+            // or the second inner loop can look like this:
+//            for (int j = 5; j >= i; j--) {
+//                System.out.print("* ");
+//            }
+
+            System.out.println();
         }
     }
 }
