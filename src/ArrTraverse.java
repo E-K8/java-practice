66db55ce37a7a0331bfff6b969725ff121ddc75f
E-K8 <ekatplat@gmail.com>
2023-07-20 12:27:54 +0100

    calculate total, average, max, min; search for number

--- src/ArrTraverse.java
+++ src/ArrTraverse.java
@@ -1,6 +1,12 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrTraverse {
+//    1. Traverse arrays
+//    2. Total, average, max, min, search for number
+
+    //    1.
+    /*
     public static void main(String[] args) {
         String[] names = {"Lucy", "Dale", "Kyle", "Chris"};
 
@@ -22,4 +28,39 @@ public class ArrTraverse {
             System.out.println(n);
         }
     }
+    */
+
+    public static void main(String[] args) {
+        double[] salaries = {23000.71, 45234.45, 63244.11, 18777.25};
+
+        double total = 0, max = salaries[0], min = salaries[0];
+
+        Scanner in = new Scanner(System.in);
+        double searchNum = in.nextDouble();
+        boolean found = false;
+
+        for (double salary : salaries) {
+            total += salary;
+            if (salary > max) {
+                max = salary;
+            }
+            if (salary < min) {
+                min = salary;
+            }
+            if (searchNum == salary) {
+                found = true;
+            }
+        }
+        if (found) {
+            System.out.println("found the match");
+        } else {
+            System.out.println("no match found");
+        }
+
+        System.out.println("Total to pay in salaries: " + String.format("%.2f",total));
+        System.out.println("Average pay per annum: " + String.format("%.2f",(total / salaries.length)));
+        System.out.println("The smallest salary being paid currently is: " + min);
+        System.out.println("The biggest salary being paid currently is: " + max);
+    }
 }
+
