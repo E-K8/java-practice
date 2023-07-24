fa3daf996bcf5619204c030f1942bf6aa8266fc8
E-K8 <ekatplat@gmail.com>
2023-07-24 17:26:02 +0100

    intro to strings

---
+++ src/StringPractice.java
@@ -0,0 +1,9 @@
+public class StringPractice {
+    public static void main(String[] args) {
+        String string1 = "Java17";
+        System.out.println(string1);
+
+        string1="Java20";
+        System.out.println(string1);
+    }
+}
