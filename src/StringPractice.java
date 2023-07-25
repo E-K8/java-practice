45ba9b9f9c15b7c4e2e26726b09002d049c05c30
E-K8 <ekatplat@gmail.com>
2023-07-25 16:38:09 +0100

    use equals instead of ==

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -3,7 +3,17 @@ public class StringPractice {
         String string1 = "Java17";
         System.out.println(string1);
 
-        string1="Java20";
+        string1 = "Java20";
         System.out.println(string1);
+
+        String myString1 = "Monday";
+        String myString2 = "Monday";
+        System.out.println(myString1.equals(myString2));
+
+        String myStr1 = new String("Tuesday");
+        String myStr2 = new String("Tuesday");
+        System.out.println(myStr1.equals(myStr2));
+        System.out.println(myStr1.equals(myStr2));
+        System.out.println(myStr2.equals(myStr1));
     }
 }
