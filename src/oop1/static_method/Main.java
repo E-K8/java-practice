9c751f168d3ee3ebc1e82e5037044c99b57104e9
E-K8 <ekatplat@gmail.com>
2023-08-06 20:42:12 +0100

    static method as explained by Alex Lee

---
+++ src/oop1/static_method/Main.java
@@ -0,0 +1,27 @@
+package oop1.static_method;
+
+import javax.print.attribute.standard.MediaSize;
+
+public class Main {
+  public static void main(String[] args) {
+    // Instance of class vs class itself:
+    // Static method: class itself
+    sayHi();
+    OtherClass.sayYes();
+
+    // Non-static method: instance of a class
+    Main m = new Main();
+    m.sayBye();
+
+    OtherClass o = new OtherClass();
+    o.sayNo();
+  }
+
+  static void sayHi() {
+    System.out.println("Hi");
+  }
+
+  void sayBye() {
+    System.out.println("Bye");
+  }
+}
---
+++ src/oop1/static_method/OtherClass.java
@@ -0,0 +1,11 @@
+package oop1.static_method;
+
+public class OtherClass {
+  static void sayYes() {
+    System.out.println("Yes");
+  }
+
+  void sayNo() {
+    System.out.println("No");
+  }
+}
