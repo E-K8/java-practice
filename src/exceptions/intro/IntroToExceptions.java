d3c05a32195c408635dc2fe29b6a22b71d84e03f
E-K8 <ekatplat@gmail.com>
2023-08-26 21:00:06 +0100

    try catch and checked exceptions

--- src/exceptions/intro/IntroToExceptions.java
+++ src/exceptions/intro/IntroToExceptions.java
@@ -1,11 +1,22 @@
 package exceptions.intro;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class IntroToExceptions {
   public static void main(String[] args) {
     try {
+      System.out.println("This is a divider before exception. Also, enter your number: ");
+
+      Scanner in = new Scanner(System.in);
+      int x = in.nextInt();
+
       int result = divide(10, 0);
+
+
       System.out.println("Result is: " + result);
-    } catch (ArithmeticException e) {
+      System.out.println("after exception");
+    } catch (Exception e) {
       System.out.println("Exception caught: " + e.getMessage());
     } finally {
       System.out.println("This block gets executed regardless of an exception.");
---
+++ src/exceptions/intro/Solution.java
@@ -0,0 +1,14 @@
+package exceptions.intro;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    System.out.println("Enter your name, please:");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String name = reader.readLine();
+    System.out.println(("You have a beautiful name, " + name + "."));
+  }
+}
