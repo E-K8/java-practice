4ae799bdc53ba1746802a992285c57cb3d9f5806
E-K8 <ekatplat@gmail.com>
2023-10-22 19:45:05 +0100

    older Calendar class use

--- src/date_time_math/Main.java
+++ src/date_time_math/Main.java
@@ -23,13 +23,47 @@ Examples of DateTime formats:
 */
 
 
+import java.sql.SQLOutput;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Main {
-  public static void main(String[] args) {
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, dd MMMM yyyy, HH:mm:ss z");
+  public static void main(String[] args) throws ParseException {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
     Date today = new Date();
     System.out.println(simpleDateFormat.format(today));
+
+    System.out.println(today.getDate());
+    System.out.println(today.getYear());
+    System.out.println(today.getMonth());
+    System.out.println(today.getDay());
+    System.out.println(today.getTime());
+
+    Date date = simpleDateFormat.parse("05/01/1977");
+    System.out.println(date);
+
+    Calendar calendar = new GregorianCalendar();
+
+    System.out.println(calendar.get(Calendar.ERA));
+    System.out.println(calendar.get(Calendar.YEAR));
+    System.out.println(calendar.get(Calendar.MONTH));
+    System.out.println(calendar.get(Calendar.WEEK_OF_YEAR));
+    System.out.println("<<<<<>>>>>");
+
+    System.out.println(calendar.get(Calendar.DATE));
+    System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+    System.out.println(calendar.get(Calendar.DAY_OF_YEAR));
+    System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
+    System.out.println("<<<<<>>>>>");
+
+    System.out.println(calendar.get(Calendar.HOUR));
+    System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+    System.out.println(calendar.get(Calendar.MINUTE));
+    System.out.println(calendar.get(Calendar.SECOND));
+    System.out.println(calendar.get(Calendar.MILLISECOND));
+    System.out.println(calendar.get(Calendar.AM_PM));
   }
 }
