f521fb2852d201c93f4cae9e9ce2e4b6c9febb31
E-K8 <ekatplat@gmail.com>
2023-11-22 06:48:40 +0000

    file operations with NIO

---
+++ src/input_output/nio/NIOReadWrite.java
@@ -0,0 +1,46 @@
+package input_output.nio;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NIOReadWrite {
+  static String str = "Working from home\n";
+  static byte[] bytes = "is good\n".getBytes();
+  static List<String> list = new ArrayList<>() {
+    {
+      add("apples");
+
+      add("oranges");
+    }
+  };
+
+  public static void main(String[] args) throws IOException {
+    Path path = Path.of("KatesFolder", "KatesFIle.txt"); // folders don't exist, so there'll be an error
+    Files.write(path, bytes, StandardOpenOption.APPEND);
+    Files.write(path, list, StandardOpenOption.APPEND);
+    Files.writeString(path, str, StandardOpenOption.APPEND);
+
+    try (OutputStream outputStream = Files.newOutputStream(path, StandardOpenOption.APPEND)) {
+      outputStream.write(65);
+    }
+    try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path, StandardOpenOption.APPEND)) {
+      bufferedWriter.write("I can't imagine any other way.");
+    }
+
+    // reading data
+
+    String s = Files.readString(path);
+    System.out.println(Files.readAllLines(path));
+    byte[] bytes1 = Files.readAllBytes(path);
+
+    InputStream inputStream =Files.newInputStream(path);
+    BufferedReader bufferedReader = Files.newBufferedReader(path);
+
+    System.out.println(inputStream);
+    System.out.println(bufferedReader);
+  }
+}
